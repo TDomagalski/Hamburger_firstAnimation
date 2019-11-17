092ab24eff8a5640e58c6403f6223f3b52b88588
Tomasz <tomekwebdev@gmail.com>
2019-11-17 16:38:35 +0100

    mouseEvent turns colors on hamburger each elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-console.log("Siema!");
+const divHam = document.querySelector(".hamburger"),
+      first = divHam.firstElementChild,
+      second = first.nextElementSibling,
+      third = second.nextElementSibling;
+
+(function() {
+
+  function assignEvents(elems, type, func) {
+
+    for(var i = 0; i < elems.length; i++) {
+      elems[i].addEventListener(type, func, false);
+    }
+
+  }
+
+  function changeColor(e) {
+    if(e.target.getAttribute('title') === "first") {
+      first.classList.add("first");
+    } else if(e.target.getAttribute('title') === "second") {
+        second.classList.add("second");
+    } else {
+        third.classList.add("third");
+    }
+  }
+
+  function defaultColor(e) {
+    first.classList.remove("first");
+    second.classList.remove("second");
+    third.classList.remove("third");
+  }
+
+  function init(elems) {
+
+      assignEvents(elems, "mouseenter", changeColor);
+      assignEvents(elems, "mouseleave", defaultColor);
+
+  }
+
+window.animati0n = init;
+
+}())
